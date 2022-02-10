c60e5cf7125057923b7cd8ac8f7f8fbc2ec17f24
iTanvir <itanvir@Tanvirs-MacBook.Dlink>
2022-02-10 22:32:18 +0600

    Done Project 1, Change Background color by button click

---
+++ 01. Project 1/app.js
@@ -0,0 +1,32 @@
+/**
+ * Project requirements
+ *  - Change the background color by generating random rgb color by clicking a button
+ */
+
+//step 1 - create onload handler
+window.onload = () => {
+	main();
+};
+
+function main() {
+	const root = document.getElementById("root");
+	const btn = document.getElementById("change-btn");
+
+	btn.addEventListener("click", function () {
+		const bgColor = generateRGBColor();
+		root.style.backgroundColor = bgColor;
+	});
+}
+
+// step 2 - random color generator function
+function generateRGBColor() {
+	const red = Math.floor(Math.random() * 255);
+	const green = Math.floor(Math.random() * 255);
+	const blue = Math.floor(Math.random() * 255);
+
+	return `rgb(${red}, ${green}, ${blue})`;
+}
+
+// step 3 - collect all necessary references
+
+// step 4 - handle the click event
