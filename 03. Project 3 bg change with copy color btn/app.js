956adec72c50dfcbc4ed7347a24f4967457bf71e
iTanvir <itanvir@Tanvirs-MacBook.Dlink>
2022-02-13 21:35:07 +0600

    done project 03 BG change with Hex Color and Copy color code btn

--- 03. Project 3 bg change with copy color btn/app.js
+++ 03. Project 3 bg change with copy color btn/app.js
@@ -1,12 +1,13 @@
 /**
  * Project requirements
  *  - Change the background color by generating random hex color by clicking a button
+ * and copy the color code
  */
 
 // step 1 - create onload handler
 // step 2 - random color generator function
 // step 3 - collect all necessary references
-// step 4 - handle the click event
+// step 4 - handle the click event and copy color code btn
 
 window.onload = () => {
 	main();
@@ -14,14 +15,21 @@ window.onload = () => {
 
 function main() {
 	const root = document.getElementById("root");
-	const btn = document.getElementById("change-btn");
 	const output = document.getElementById("output");
+	const changeBtn = document.getElementById("change-btn");
+	const copyBtn = document.getElementById("copy-btn");
 
-	btn.addEventListener("click", function () {
+	// change color btn
+	changeBtn.addEventListener("click", function () {
 		const bgColor = generateHexColor();
 
 		root.style.backgroundColor = bgColor;
 		output.value = bgColor;
+	});
+
+	// copy color code btn
+	copyBtn.addEventListener("click", function () {
+		window.navigator.clipboard.writeText(output.value);
 	});
 }
 
